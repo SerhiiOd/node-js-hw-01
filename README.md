e2eee8b901c96ed9a67bc6862d0fe3ad1581df60
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-07-30 16:17:48 +0300

    upd code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-1. https://ibb.co/3p2Zrb1
-2. https://ibb.co/J2Gx3ph
-3. https://ibb.co/s9ZsCR7
-4. https://ibb.co/sPC1Lq4
+1. https://ibb.co/Swrh5nQ
+2. https://ibb.co/5jN3hJ9
+3. https://ibb.co/y41W69w
+4. https://ibb.co/x5YKP3M
--- index.js
+++ index.js
@@ -17,22 +17,22 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
       break;
 
     case "get":
       const oneContact = await contacts.getContactById(id);
-      console.log(oneContact);
+      console.table(oneContact);
       break;
 
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
-      console.log(newContact);
+      console.table(newContact);
       break;
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
-      console.log(removeContact);
+      console.table(removeContact);
       break;
 
     default:
